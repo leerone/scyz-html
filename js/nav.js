15fa819579b4ee152cc8897ce84a5af75b88e274
Chen Lilang <chenll@suncd.com>
2018-04-20 14:34:54 +0800

    菜单的露白处理

--- js/nav.js
+++ js/nav.js
@@ -1,10 +1,45 @@
+/*
+ * $.hover事件的延时处理
+ **/
+(function($){
+    $.fn.hoverDelay = function(options){
+        var defaults = {
+            hoverDuring: 200,
+            outDuring: 200,
+            hoverEvent: function(){
+                $.noop();
+            },
+            outEvent: function(){
+                $.noop();    
+            }
+        };
+        var sets = $.extend(defaults,options || {});
+        var hoverTimer, outTimer;
+        return this.each(function() {
+            $(this).hover(function() {
+                clearTimeout(outTimer);
+                hoverTimer = setTimeout(sets.hoverEvent, sets.hoverDuring);
+            }, function() {
+                clearTimeout(hoverTimer);
+                outTimer = setTimeout(sets.outEvent, sets.outDuring);
+            });    
+        });
+    }      
+})(jQuery);
+
 $(function() {
+	var enterTimer;
 	$('#mainNav li.item').hover(function() {
-		$('#mainNav li.item').removeClass('active');
-		$(this).addClass('active');
-		$(this).find('ul.sub-nav').slideDown();
+		var self = this;
+		enterTimer = setTimeout(function() {
+			$('#mainNav li.item').removeClass('active');
+			$(self).addClass('active');
+			$(self).find('ul.sub-nav').slideDown();
+		}, 200);
 	}, function() {
-		$(this).find('ul.sub-nav').slideUp();
+		var self = this;
+		clearTimeout(enterTimer);
+		$(self).find('ul.sub-nav').slideUp();
 	});
 
 	$('#mainNav li.item ul.sub-nav li a').click(function(){
